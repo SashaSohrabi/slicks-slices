bdbeb1319ff23da38643188000b24e13100673a7
Sasha Sohrabi <sasha@nxte.nl>
2020-10-15 22:27:43 +0300

    styling the Logo

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import styled from 'styled-components';
+import Logo from '../components/Logo';
 
 const NavStyles = styled.nav`
   margin-bottom: 3rem;
@@ -51,7 +52,9 @@ export default function Nav() {
           <Link to="/pizzas/">Pizza Menu</Link>
         </li>
         <li>
-          <Link to="/">Logo</Link>
+          <Link to="/">
+            <Logo />
+          </Link>
         </li>
         <li>
           <Link to="/slicemasters">SliceMasters</Link>
