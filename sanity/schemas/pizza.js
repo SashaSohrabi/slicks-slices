ae71d7c94c23265724dce42b0bbf8f6cd7b85b72
Sasha Sohrabi <sasha@nxte.nl>
2020-10-19 17:18:20 +0300

    Custom CMS Inputs in Sanity

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,4 +1,5 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
   // Computer Name
@@ -36,14 +37,15 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: Rule => Rule.min(1000).max(50000)
-      // TODO: Add custom input component
+      validation: Rule => Rule.min(1000).max(50000),
+      // Custom input component
+      inputComponent: PriceInput
     },
     {
       name: 'toppings',
       title: 'Toppings',
       type: 'array',
-      of: [{type: 'reference', to: [{type: 'topping'}]}]
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }]
     }
   ],
   preview: {
@@ -53,17 +55,19 @@ export default {
       topping0: 'toppings.0.name',
       topping1: 'toppings.1.name',
       topping2: 'toppings.2.name',
-      topping3: 'toppings.3.name',
+      topping3: 'toppings.3.name'
     },
-    prepare: ({title, media, ...toppings}) => {
+    prepare: ({ title, media, ...toppings }) => {
       // 1. Filter undefined toppings out
-      const tops = Object.values(toppings).filter(topping => topping !== undefined)
+      const tops = Object.values(toppings).filter(
+        topping => topping !== undefined
+      );
       // 2. Return the preview object for the pizza
       return {
         title,
         media,
-        subtitle: tops.join(', '),
-      }
+        subtitle: tops.join(', ')
+      };
     }
   }
 };
