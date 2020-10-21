caa90084ed7450a46e6d5d790a2d4b09a0f0cce1
Sasha Sohrabi <sasha@nxte.nl>
2020-10-21 17:54:57 +0300

    sourcing Sanity Data and GraphQL

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,24 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '.env' });
+
 module.exports = {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: `https://gatsby.pizza`,
     description: `The best pizza place in Hamilton!`
-  }
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '9ygprscc',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN
+      }
+    }
+  ]
 };
