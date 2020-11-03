9cf3014df1e5eba57289469745ac6684c89d1c7a
Sasha Sohrabi <sasha@nxte.nl>
2020-11-03 17:21:19 +0200

    gatsby query

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,5 +1,34 @@
-import React from 'react';
-import Layout from '../components/Layout';
-export default function PizzasPage() {
-  return <p>Pizzas Page</p>;
+import React, { useState } from 'react';
+import { graphql } from 'gatsby';
+
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
+
+  return (
+    <>
+      <p>There are {pizzas.length} Pizzas!!!</p>
+    </>
+  );
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        price
+        image {
+          asset {
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
