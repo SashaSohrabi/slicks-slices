c9cfee51a5ddd01377d3fc36dbcccac63a20481a
sashasohrabi <shb.sohrabi@gmail.com>
2021-01-24 00:16:21 +0200

    dynamically creating pages with gatsby-node

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,40 @@
+const path = require(`path`);
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  // 2. Query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // 3. Loop over each pizza and create a page for that pizza
+  data.pizzas.nodes.forEach(pizza => {
+    actions.createPage({
+      // What si the URL for this page?
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      // pass data to component
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+// Create blog pages dynamically
+exports.createPages = async params => {
+  // Create pages dynamically
+  // 1. Pizzas
+  await turnPizzasIntoPages(params);
+  // 2. Toppings
+  // 3. Slice masters
+};
