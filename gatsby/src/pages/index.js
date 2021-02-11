a81968059ecc4fbe332b5344fc89d3c2fa2b5e41
sashasohrabi <shb.sohrabi@gmail.com>
2021-02-11 21:28:54 +0200

    Creating a Skeleton Screen while Loading Items

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
+import { HomePageGrid, ItemsGrid } from '../styles/Grids';
 import useLatestData from '../utils/useLatestData';
 // import { HomePageGrid } from '../styles/Grids';
-// import LoadingGrid from '../components/LoadingGrid';
 // import ItemGrid from '../components/ItemGrid';
 
 function CurrentlySlicing({ slicemasters }) {
@@ -10,12 +11,11 @@ function CurrentlySlicing({ slicemasters }) {
       <h2 className="center">
         <span className="mark tilt">Slicemasters On</span>
       </h2>
-      {/* <p>Standing by, ready to slice you up!</p>
+      <p>Standing by, ready to slice you up!</p>
       {!slicemasters && <LoadingGrid count={4} />}
       {slicemasters && !slicemasters?.length && (
         <p>No one is working right now!</p>
       )}
-      {slicemasters?.length && <ItemGrid items={slicemasters} />} */}
     </div>
   );
 }
@@ -25,10 +25,11 @@ function HotSlices({ hotSlices }) {
       <h2 className="center">
         <span className="mark tilt">Hot Slices!</span>
       </h2>
-      {/* <p>Come on by, buy the slice!</p>
+      <p>Come on by, buy the slice!</p>
       {!hotSlices && <LoadingGrid count={4} />}{' '}
       {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
-      {hotSlices?.length && <ItemGrid items={hotSlices} />} */}
+      {/* {hotSlices?.length && <ItemsGrid items={hotSlices} />}
+      <LoadingGrid count={4} /> */}
     </div>
   );
 }
@@ -39,10 +40,10 @@ export default function HomePage() {
     <div className="center">
       <h1>The Best Pizza Downtown!</h1>
       <p>Open 11am to 11pm Every Single Day</p>
-      <div>
+      <HomePageGrid>
         <CurrentlySlicing slicemasters={slicemasters} />
         <HotSlices hotSlices={hotSlices} />
-      </div>
+      </HomePageGrid>
     </div>
   );
 }
